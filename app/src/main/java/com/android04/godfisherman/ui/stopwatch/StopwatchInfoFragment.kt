514f63e65b6cf31071b35439f9096970909123cc
potatoyum <46017398+potatoyum@users.noreply.github.com>
2021-11-02 14:32:37 +0900

    Feat: 하단 내비게이션 설정

---
+++ app/src/main/java/com/android04/godfisherman/ui/stopwatch/StopwatchInfoFragment.kt
@@ -0,0 +1,19 @@
+package com.android04.godfisherman.ui.stopwatch
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android04.godfisherman.R
+
+class StopwatchInfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_stopwatch_info, container, false)
+    }
+}
