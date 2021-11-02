56160308a73050695c1a5817567b3ec80eb301fe
sejins <pengang1011@naver.com>
2021-11-03 01:56:47 +0900

    Feat: SharedPreferenceManager 생성

---
+++ app/src/main/java/com/android04/godfisherman/utils/SharedPreferenceManager.kt
@@ -0,0 +1,26 @@
+package com.android04.godfisherman.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class SharedPreferenceManager @Inject constructor(
+    @ApplicationContext context: Context
+){
+    private val sharedPref: SharedPreferences =
+        context.getSharedPreferences(PREF_APP_FILE, Context.MODE_PRIVATE)
+
+
+    fun saveString(key: String, value: String) {
+        val editor = sharedPref.edit()
+        editor.putString(key, value).apply()
+    }
+
+    fun getString(key: String) = sharedPref.getString(key, "")
+
+    companion object {
+        const val PREF_APP_FILE = "pref_app_file"
+        const val PREF_LOCATION = "pref_location"
+    }
+}
