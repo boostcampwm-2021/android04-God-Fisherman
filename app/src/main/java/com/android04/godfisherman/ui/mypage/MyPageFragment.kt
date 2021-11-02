3d4cce3ee26fba5e2de219407014ec49664be1a8
potatoyum <46017398+potatoyum@users.noreply.github.com>
2021-11-02 14:32:37 +0900

    Feat: 하단 내비게이션 설정

---
+++ app/src/main/java/com/android04/godfisherman/ui/mypage/MyPageFragment.kt
@@ -0,0 +1,19 @@
+package com.android04.godfisherman.ui.mypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android04.godfisherman.R
+
+class MyPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    }
+}
