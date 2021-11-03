68961cbc031945bb1ad8a4459ecbfe1f36b88c5f
sejins <pengang1011@naver.com>
2021-11-03 22:11:28 +0900

    Feat: demo 용 저장된 위치 표시하는 Toast 출력

--- app/src/main/java/com/android04/godfisherman/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android04/godfisherman/ui/home/HomeFragment.kt
@@ -3,11 +3,13 @@ package com.android04.godfisherman.ui.home
 import android.Manifest
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.viewModels
 import com.android04.godfisherman.R
 import com.android04.godfisherman.databinding.FragmentHomeBinding
 import com.android04.godfisherman.ui.base.BaseFragment
+import com.android04.godfisherman.utils.SharedPreferenceManager
 import com.android04.godfisherman.utils.isGrantedLocationPermission
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,6 +27,13 @@ class HomeFragment : BaseFragment<FragmentHomeBinding,HomeViewModel>(R.layout.fr
     private fun updateLocation() {
         if (isGrantedLocationPermission(requireContext())) {
             viewModel.updateLocation()
+            // demo 용 저장된 위치 표시하는 Toast
+            Toast.makeText(
+                requireContext(),
+                SharedPreferenceManager(requireContext()).getString(SharedPreferenceManager.PREF_LOCATION),
+                Toast.LENGTH_SHORT
+            ).show()
+
         } else {
             ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
         }
