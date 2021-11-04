fd88e89519bc0b26649e193df52c86f4ff66d6c7
JeonJongHyeon97 <ziziwhdgus@naver.com>
2021-11-04 17:55:57 +0900

    Feat: 수평계 flag 설정

--- app/src/main/java/com/android04/godfisherman/ui/camera/CameraViewModel.kt
+++ app/src/main/java/com/android04/godfisherman/ui/camera/CameraViewModel.kt
@@ -1,5 +1,18 @@
 package com.android04.godfisherman.ui.camera
 
+import android.hardware.SensorEvent
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.math.abs
+import kotlin.math.roundToInt
 
-class CameraViewModel : ViewModel()
+class CameraViewModel : ViewModel(){
+    private val _isLevelOk = MutableLiveData<Boolean>(false)
+    val isLevelOk: LiveData<Boolean> = _isLevelOk
+
+    fun changedLevel(event: SensorEvent){
+        _isLevelOk.value = abs(event.values[0].roundToInt()) <= 1 && abs(event.values[1].roundToInt()) <= 1
+    }
+
+}
