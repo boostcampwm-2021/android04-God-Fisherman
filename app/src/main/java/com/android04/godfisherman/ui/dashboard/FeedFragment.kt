5bc7722ce97daeffafbe57841e1b3533c2a90e89
chiyoon <chiyoon98@kaist.ac.kr>
2021-11-10 20:42:17 +0900

    Design: Feed 기본 UI 구현

---
+++ app/src/main/java/com/android04/godfisherman/ui/dashboard/FeedFragment.kt
@@ -0,0 +1,10 @@
+package com.android04.godfisherman.ui.dashboard
+
+import androidx.fragment.app.viewModels
+import com.android04.godfisherman.R
+import com.android04.godfisherman.databinding.FragmentFeedBinding
+import com.android04.godfisherman.ui.base.BaseFragment
+
+class FeedFragment : BaseFragment<FragmentFeedBinding, FeedViewModel>(R.layout.fragment_feed) {
+    override val viewModel: FeedViewModel by viewModels()
+}
