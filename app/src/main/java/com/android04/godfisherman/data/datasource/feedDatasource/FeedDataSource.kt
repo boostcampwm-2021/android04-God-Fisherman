17af2bd401614458327c4f897604846aea3a3203
potatoyum <46017398+potatoyum@users.noreply.github.com>
2021-11-11 21:46:59 +0900

    Feat: FeedRemoteDataSourceImpl - 피드 데이터 요청

--- app/src/main/java/com/android04/godfisherman/data/datasource/feedDatasource/FeedDataSource.kt
+++ app/src/main/java/com/android04/godfisherman/data/datasource/feedDatasource/FeedDataSource.kt
@@ -1,11 +1,15 @@
 package com.android04.godfisherman.data.datasource.feedDatasource
 
+import com.android04.godfisherman.data.entity.FishingRecord
+import com.android04.godfisherman.data.entity.Type
+
 interface FeedDataSource {
 
     interface LocalDataSource {
     }
 
     interface RemoteDataSource {
+        suspend fun fetchFeedDataList(): List<Pair<Type, List<FishingRecord>>>?
     }
 
 }
--- app/src/main/java/com/android04/godfisherman/data/datasource/feedDatasource/remote/FeedRemoteDataSourceImpl.kt
+++ app/src/main/java/com/android04/godfisherman/data/datasource/feedDatasource/remote/FeedRemoteDataSourceImpl.kt
@@ -1,7 +1,55 @@
 package com.android04.godfisherman.data.datasource.feedDatasource.remote
 
 import com.android04.godfisherman.data.datasource.feedDatasource.FeedDataSource
+import com.android04.godfisherman.data.entity.FishingRecord
+import com.android04.godfisherman.data.entity.Type
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class FeedRemoteDataSourceImpl @Inject constructor() : FeedDataSource.RemoteDataSource {
+    private val database = Firebase.firestore
+
+    override suspend fun fetchFeedDataList(): List<Pair<Type, List<FishingRecord>>> {
+        val result = mutableListOf<Pair<Type, List<FishingRecord>>>()
+
+        val feedRef = database.collection("Feed")
+
+        var feedDocs: List<DocumentSnapshot>? = null
+        feedRef.get().addOnSuccessListener {
+            feedDocs = it.documents
+        }.await()
+
+        feedDocs?.let { feedList ->
+            feedList.forEach { feed ->
+                val feedType: Type? = feed.toObject<Type>()
+
+                feed.reference.collection("fishingRecord").get()
+                    .addOnSuccessListener { docs ->
+                        val fishingRecordList = mutableListOf<FishingRecord>()
+
+                        docs.documents.forEach { recordDoc ->
+                            val fishingRecord = recordDoc.toObject<FishingRecord>()
+
+                            if (fishingRecord != null) {
+                                fishingRecordList.add(fishingRecord)
+                            }
+                        }
+
+                        if (feedType != null && fishingRecordList.isNotEmpty()) {
+                            result.add(Pair(feedType, fishingRecordList.toList()))
+                        }
+
+                    }.addOnFailureListener {
+                        //Todo
+                    }
+                    .await()
+            }
+        }
+
+        return result
+    }
 }
