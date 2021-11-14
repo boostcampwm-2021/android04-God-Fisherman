b6d1d1e74a7d387361b29f7bc0f266e9f7c6ef83
chiyoon <chiyoon98@kaist.ac.kr>
2021-11-15 02:21:06 +0900

    Fix: 사진 업로드 성공, 실패 처리 추가

--- app/src/main/java/com/android04/godfisherman/data/repository/UploadRepository.kt
+++ app/src/main/java/com/android04/godfisherman/data/repository/UploadRepository.kt
@@ -5,7 +5,9 @@ import com.android04.godfisherman.data.datasource.uploadDataSource.UploadDataSou
 import com.android04.godfisherman.data.entity.FishingRecord
 import com.android04.godfisherman.data.entity.Type
 import com.android04.godfisherman.localdatabase.entity.TmpFishingRecord
+import com.android04.godfisherman.utils.RepoResponse
 import com.google.firebase.Timestamp
+import java.lang.Exception
 import java.util.*
 import javax.inject.Inject
 
@@ -16,20 +18,36 @@ class UploadRepository @Inject constructor(
 
     suspend fun fetchFishTypeList(): List<String> = remoteDataSource.fetchFishTypeList()
 
-    suspend fun saveImageType(image: Bitmap, fishLength: Double, fishType: String) {
+    suspend fun saveImageType(image: Bitmap, fishLength: Double, fishType: String, callback: RepoResponse<Unit>) {
         val imageUrl = remoteDataSource.getImageUrl(image)
 
         imageUrl?.let {
             val type = Type(Timestamp(Date()),false, "", 0, "user1")
             val fishingRecord = FishingRecord(0, imageUrl, Date(), fishLength, fishType)
+            var isSuccess = true
 
-            remoteDataSource.saveImageType(type, fishingRecord)
+            try {
+                remoteDataSource.saveImageType(type, fishingRecord)
+            } catch (e: Exception) {
+                isSuccess = false
+            } finally {
+                callback.invoke(isSuccess, Unit)
+            }
         }
     }
 
-    suspend fun saveTmpTimeLineRecord(image: Bitmap, fishLength: Double, fishType: String){
+    suspend fun saveTmpTimeLineRecord(image: Bitmap, fishLength: Double, fishType: String, callback: RepoResponse<Unit>){
         val fishingRecord = TmpFishingRecord(image, Date(), fishLength, fishType)
-        localDataSource.saveTmpTimeLineRecord(fishingRecord)
+        var isSuccess = true
+
+        try {
+            localDataSource.saveTmpTimeLineRecord(fishingRecord)
+        } catch (e: Exception) {
+            isSuccess = false
+        } finally {
+            callback.invoke(isSuccess, Unit)
+        }
+
     }
 
 }
