e9c45e1e74f792dda5b0a3f3d0ff0b8abd5405c9
chiyoon <chiyoon98@kaist.ac.kr>
2021-11-16 00:08:39 +0900

    Feat: 인터넷 연결 체크 싱글턴 추가

---
+++ app/src/main/java/com/android04/godfisherman/common/NetworkChecker.kt
@@ -0,0 +1,23 @@
+package com.android04.godfisherman.common
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+object NetworkChecker {
+
+    fun isConnected (context: Context): Boolean {
+        val manager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val network = manager.activeNetwork ?: return false
+        val networkCapabilities = manager.getNetworkCapabilities(network) ?: return false
+
+        return when {
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
+
+}
