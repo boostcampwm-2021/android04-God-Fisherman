d1949c4c77baca26c5db7a2442afd798946015e2
potatoyum <46017398+potatoyum@users.noreply.github.com>
2021-11-16 21:25:12 +0900

    Feat: Room - 피드 데이터 모델링

--- app/src/main/java/com/android04/godfisherman/localdatabase/LocalDatabase.kt
+++ app/src/main/java/com/android04/godfisherman/localdatabase/LocalDatabase.kt
@@ -3,13 +3,22 @@ package com.android04.godfisherman.localdatabase
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.android04.godfisherman.localdatabase.dao.FeedCachedDao
 import com.android04.godfisherman.localdatabase.dao.TmpFishingRecordDao
+import com.android04.godfisherman.localdatabase.entity.FishingRecordCached
 import com.android04.godfisherman.localdatabase.entity.TmpFishingRecord
+import com.android04.godfisherman.localdatabase.entity.TypeInfoCached
+import org.w3c.dom.TypeInfo
 
-@Database(entities = [TmpFishingRecord::class], version = 1, exportSchema = false)
+@Database(
+    entities = [TmpFishingRecord::class, TypeInfoCached::class, FishingRecordCached::class],
+    version = 1,
+    exportSchema = false
+)
 @TypeConverters(DatabaseTypeConverter::class)
 abstract class LocalDatabase : RoomDatabase() {
 
     abstract fun temporaryFishingRecordDao(): TmpFishingRecordDao
 
+    abstract fun feedCachedDao(): FeedCachedDao
 }
