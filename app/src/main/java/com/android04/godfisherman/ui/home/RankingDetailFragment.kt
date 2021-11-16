b126607e82803337bfb7ad1f1ea1f304dac77adb
sejins <pengang1011@naver.com>
2021-11-17 01:12:31 +0900

    Design: 랭킹 더보기화면 UI 구현

--- app/src/main/java/com/android04/godfisherman/ui/home/RankingDetailFragment.kt
+++ app/src/main/java/com/android04/godfisherman/ui/home/RankingDetailFragment.kt
@@ -1,6 +1,6 @@
 package com.android04.godfisherman.ui.home
 
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import com.android04.godfisherman.R
 import com.android04.godfisherman.databinding.FragmentRankingDetailBinding
 import com.android04.godfisherman.ui.base.BaseFragment
@@ -8,6 +8,6 @@ import com.android04.godfisherman.ui.base.BaseFragment
 
 class RankingDetailFragment : BaseFragment<FragmentRankingDetailBinding, HomeViewModel>(R.layout.fragment_ranking_detail) {
 
-    override val viewModel: HomeViewModel by viewModels()
+    override val viewModel: HomeViewModel by activityViewModels()
 
 }
