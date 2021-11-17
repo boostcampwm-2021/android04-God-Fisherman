a6f32ed7ed927298d179ba05d5e8626fce864d8d
potatoyum <46017398+potatoyum@users.noreply.github.com>
2021-11-17 16:42:33 +0900

    Refactor: FeedRepository - Mapper 별도 분리

--- app/src/main/java/com/android04/godfisherman/data/repository/FeedRepository.kt
+++ app/src/main/java/com/android04/godfisherman/data/repository/FeedRepository.kt
@@ -4,17 +4,9 @@ import com.android04.godfisherman.common.NetworkChecker
 import com.android04.godfisherman.common.Type
 import com.android04.godfisherman.data.DTO.FeedDTO
 import com.android04.godfisherman.data.datasource.feedDatasource.FeedDataSource
-import com.android04.godfisherman.localdatabase.entity.FishingRecordCached
-import com.android04.godfisherman.localdatabase.entity.TypeInfoCached
 import com.android04.godfisherman.ui.feed.FeedData
-import com.android04.godfisherman.ui.feed.FeedPhotoData
-import com.android04.godfisherman.ui.feed.FeedTimelineData
-import com.android04.godfisherman.ui.feed.TimeLineData
-import com.android04.godfisherman.utils.getFeedDateFormat
-import com.android04.godfisherman.utils.getFeedTimeFormat
+import com.android04.godfisherman.utils.*
 import kotlinx.coroutines.*
-import java.text.SimpleDateFormat
-import java.util.*
 import javax.inject.Inject
 
 class FeedRepository @Inject constructor(
@@ -30,97 +22,20 @@ class FeedRepository @Inject constructor(
         }
     }
 
-    private suspend fun loadFeedDataList(type: Type): List<FeedData> {
-        val list = mutableListOf<FeedData>()
-
-        localDataSource.loadFeedDataList()?.forEach { feed ->
-            when (feed.typeInfo.isTimeline) {
-                true -> {
-                    val typeInfo = feed.typeInfo
-
-                    list.add(
-                        FeedTimelineData(
-                            typeInfo.userName,
-                            typeInfo.location,
-                            getFeedDateFormat(typeInfo.id),
-                            feed.fishingRecords.map { it.imageUrl },
-                            feed.fishingRecords.map {
-                                TimeLineData(
-                                    it.fishType,
-                                    it.fishLength,
-                                    getFeedTimeFormat(it.date)
-                                )
-                            })
-                    )
-                }
-                false -> {
-                    val typeInfo = feed.typeInfo
-                    val photo = feed.fishingRecords[0]
-
-                    list.add(
-                        FeedPhotoData(
-                            typeInfo.userName,
-                            typeInfo.location,
-                            getFeedDateFormat(typeInfo.id),
-                            photo.imageUrl,
-                            photo.fishType,
-                            photo.fishLength
-                        )
-                    )
-                }
-            }
-        }
-
-        return list
-    }
-
     private suspend fun fetch(type: Type): List<FeedData> {
         val list = mutableListOf<FeedData>()
-
         val feedList = remoteDataSource.fetchFeedDataList(type)
 
         if (feedList != null) {
             feedList.forEach { feed ->
                 when (feed.typeInfo.isTimeline) {
-                    true -> {
-                        val typeInfo = feed.typeInfo
-
-                        list.add(
-                            FeedTimelineData(
-                                typeInfo.userName,
-                                typeInfo.location,
-                                getFeedDateFormat(typeInfo.id.toDate()),
-                                feed.fishingRecordList.map { it.imageUrl },
-                                feed.fishingRecordList.map {
-                                    TimeLineData(
-                                        it.fishType,
-                                        it.fishLength,
-                                        getFeedTimeFormat(it.date)
-                                    )
-                                }
-                            )
-                        )
-                    }
-                    false -> {
-                        val typeInfo = feed.typeInfo
-                        val photo = feed.fishingRecordList[0]
-
-                        list.add(
-                            FeedPhotoData(
-                                typeInfo.userName,
-                                typeInfo.location,
-                                getFeedDateFormat(typeInfo.id.toDate()),
-                                photo.imageUrl,
-                                photo.fishType,
-                                photo.fishLength
-                            )
-                        )
-                    }
+                    true -> list.add(feed.toFeedTimeLineData())
+                    false -> list.add(feed.toFeedPhotoData())
                 }
             }
 
             CoroutineScope(Dispatchers.IO + NonCancellable).launch {
-                saveFeedList(feedList)
+                saveFeedListInCache(feedList)
             }
         } else {
             //todo
@@ -129,22 +44,26 @@ class FeedRepository @Inject constructor(
         return list
     }
 
-    private suspend fun saveFeedList(feedList: List<FeedDTO>) {
+
+    private suspend fun loadFeedDataList(type: Type): List<FeedData> {
+        val list = mutableListOf<FeedData>()
+
+        localDataSource.loadFeedDataList()?.forEach { feed ->
+            when (feed.typeInfo.isTimeline) {
+                true -> list.add(feed.toFeedTimeLineData())
+                false -> list.add(feed.toFeedPhotoData())
+            }
+        }
+
+        return list
+    }
+
+    private suspend fun saveFeedListInCache(feedList: List<FeedDTO>) {
         feedList.forEach { feed ->
             localDataSource.saveFeed(
-                feed.typeInfo.run {
-                    TypeInfoCached(id.toDate(), isTimeline, location, fishingTime, userName)
-                },
-
+                feed.typeInfo.toTypeInfoCached(),
                 feed.fishingRecordList.map {
-                    FishingRecordCached(
-                        feed.typeInfo.id.toDate(),
-                        it.id,
-                        it.imageUrl,
-                        it.date,
-                        it.fishLength,
-                        it.fishType
-                    )
+                    it.toFishingRecordCached(feed.typeInfo)
                 }
             )
         }
