c3cdcc34f430c5a0a3cd4a18bd7a775db4409637
potatoyum <46017398+potatoyum@users.noreply.github.com>
2021-11-22 23:02:03 +0900

    Fix: Result 클래스 수정

--- app/src/main/java/com/android04/godfisherman/common/Result.kt
+++ app/src/main/java/com/android04/godfisherman/common/Result.kt
@@ -1,6 +1,6 @@
 package com.android04.godfisherman.common
 
-sealed class Result<T> {
+sealed class Result<out T> {
     data class Success<T>(val data: T) : Result<T>()
-    data class Fail<T>(val description: String) : Result<T>()
+    data class Fail(val description: String) : Result<Nothing>()
 }
