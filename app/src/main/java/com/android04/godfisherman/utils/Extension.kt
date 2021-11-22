46a0c591952594f65cf25417149a678ee4511589
JeonJongHyeon97 <ziziwhdgus@naver.com>
2021-11-22 16:01:24 +0900

    Feat: 시간 표시 수정

--- app/src/main/java/com/android04/godfisherman/utils/Extension.kt
+++ app/src/main/java/com/android04/godfisherman/utils/Extension.kt
@@ -13,20 +13,27 @@ fun ByteBuffer.toByteArray(): ByteArray {
 }
 
 fun Double.toTimeMilliSecond(): String{
-    val time = this.roundToInt() % 8640000
-    val hour = time / 360000
-    val min = time % 3600000 / 6000
-    val sec = time % 3600000 % 6000 / 100
-    val milli = time % 3600000 % 6000 % 100
+    val time = this.roundToInt()
+    val hour = time / 3600000
+    val min = time % 3600000 / 60000
+    val sec = time % 3600000 % 60000 / 1000
+    val milli = time % 3600000 % 60000 % 1000
     return  String.format("%02d:%02d:%02d:%02d", hour, min, sec, milli)
 }
 
 fun Double.toTimeSecond(): String{
-    val time = this.roundToInt() % 8640000
-    val hour = time / 360000
-    val min = time % 3600000 / 6000
-    val sec = time % 3600000 % 6000 / 100
+    val time = this.roundToInt()
+    val hour = time / 3600000
+    val min = time % 3600000 / 60000
+    val sec = time % 3600000 % 60000 / 1000
     return  String.format("%02d:%02d:%02d", hour, min, sec)
+}
+
+fun Double.toTimeHourMinute(): String{
+    val time = this.roundToInt()
+    val hour = time / 3600000
+    val min = time % 3600000 / 60000
+    return  String.format("%02d시간 %02d분", hour, min)
 }
 
 fun Date.toDateString(): String{
