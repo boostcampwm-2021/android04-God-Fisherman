9e9f01d39f2b191e2e961137d07a8ab9f2d1ea97
sejins <pengang1011@naver.com>
2021-11-22 21:26:22 +0900

    Fix: 홈화면의 사용자 개인 랭킹 텍스트 수정

--- app/src/main/java/com/android04/godfisherman/utils/BindingAdapter.kt
+++ app/src/main/java/com/android04/godfisherman/utils/BindingAdapter.kt
@@ -138,10 +138,4 @@ object BindingAdapter {
     fun setWelcomeTextWithID(view: TextView, id: String) {
         view.text = "안녕하세요 ${id}님!"
     }
-
-    @JvmStatic
-    @BindingAdapter("setRankID")
-    fun setRankingTextWithIDAndRank(view: TextView, id: String) {
-        view.text = "이번 주 ${id}님의 전체 순위는 10위 입니다."
-    }
 }
