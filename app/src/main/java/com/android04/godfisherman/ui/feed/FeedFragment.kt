4eb1f5ae729a4512c529fe1190ff4f8693e0d5f0
JeonJongHyeon97 <ziziwhdgus@naver.com>
2021-11-23 23:00:35 +0900

    Refactor: data binding 및 중복 코드 제거

--- app/src/main/java/com/android04/godfisherman/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/android04/godfisherman/ui/feed/FeedFragment.kt
@@ -13,7 +13,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class FeedFragment : BaseFragment<FragmentFeedBinding, FeedViewModel>(R.layout.fragment_feed) {
@@ -21,76 +20,42 @@ class FeedFragment : BaseFragment<FragmentFeedBinding, FeedViewModel>(R.layout.f
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.feedFragment = this
         binding.rvFeed.adapter = FeedAdapter()
         binding.feedViewModel = viewModel
         setStatusBarColor(R.color.basic)
-
-        setRefresh()
         initListener()
         initRecyclerView()
     }
 
-    private fun setRefresh(){
-        binding.SRLFeed.setOnRefreshListener {
-            binding.rvFeed.adapter = FeedAdapter()
-            pagingStateListener()
-            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-                viewModel.setFilter(binding.cgType.checkedChipId).collectLatest { newData ->
-                    Log.d("paging3", "collect : $newData")
-                    (binding.rvFeed.adapter as FeedAdapter).submitData(newData)
-                }
-            }
-        }
-    }
-
     private fun initListener() {
         binding.cgType.setOnCheckedChangeListener { _, checkedId ->
-            binding.rvFeed.adapter = FeedAdapter()
-            pagingStateListener()
-            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-                viewModel.setFilter(checkedId).collectLatest { newData ->
-                    Log.d("paging3", "collect : $newData")
-                    (binding.rvFeed.adapter as FeedAdapter).submitData(newData)
-                }
-            }
+            initRecyclerView()
+        }
+        binding.SRLFeed.setOnRefreshListener {
+            initRecyclerView()
         }
     }
 
-    private fun initRecyclerView(){
+    private fun initRecyclerView() {
+        binding.rvFeed.adapter = FeedAdapter()
+        pagingStateListener()
         viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-            binding.rvFeed.adapter = FeedAdapter()
-            pagingStateListener()
             viewModel.setFilter(binding.cgType.checkedChipId).collectLatest { newData ->
                 (binding.rvFeed.adapter as FeedAdapter).submitData(newData)
             }
         }
     }
 
-    private fun pagingStateListener(){
+    private fun pagingStateListener() {
         viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
             (binding.rvFeed.adapter as FeedAdapter).loadStateFlow.collectLatest { loadStates ->
-                when(loadStates.refresh){
+                when (loadStates.refresh) {
                     is LoadState.Loading -> {
-                        Log.d("LoadState", "로딩 중")
-                        withContext(Dispatchers.Main){
-                            binding.lottieLoading.visibility = View.VISIBLE
-                            binding.lottieLoading.playAnimation()
-                            binding.cpTypeAll.isEnabled = false
-                            binding.cpTypePhoto.isEnabled = false
-                            binding.cpTypeTimeline.isEnabled = false
-                        }
+                        viewModel.setLoadingOn()
                     }
                     !is LoadState.Loading -> {
-                        Log.d("LoadState", "로딩 끝")
-                        withContext(Dispatchers.Main){
-                            binding.lottieLoading.pauseAnimation()
-                            binding.lottieLoading.visibility = View.GONE
-                            binding.SRLFeed.isRefreshing = false
-                            binding.cpTypeAll.isEnabled = true
-                            binding.cpTypePhoto.isEnabled = true
-                            binding.cpTypeTimeline.isEnabled = true
-                        }
+                        viewModel.setLoadingOff()
                     }
                     is LoadState.Error -> {
                         Log.d("LoadState", "에러 발생")
@@ -98,6 +63,6 @@ class FeedFragment : BaseFragment<FragmentFeedBinding, FeedViewModel>(R.layout.f
                 }
             }
         }
-
     }
+
 }
