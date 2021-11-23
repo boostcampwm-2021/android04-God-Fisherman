c2b61345f120c01228b6eb533bb9ad8ae7f1afad
chiyoon <chiyoon98@kaist.ac.kr>
2021-11-23 14:37:42 +0900

    Refactor: 파이어베이스 로그인 매니저로 리팩토링

--- app/src/main/java/com/android04/godfisherman/ui/login/LogInViewModel.kt
+++ app/src/main/java/com/android04/godfisherman/ui/login/LogInViewModel.kt
@@ -3,12 +3,15 @@ package com.android04.godfisherman.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.android04.godfisherman.utils.SharedPreferenceManager
+import com.android04.godfisherman.data.repository.LogInRepository
+import com.android04.godfisherman.utils.RepoResponseImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class LogInViewModel @Inject constructor(private val manager: SharedPreferenceManager) : ViewModel() {
+class LogInViewModel @Inject constructor(
+    private val repository: LogInRepository
+) : ViewModel() {
 
     private val _isLogin: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
     val isLogin: LiveData<Boolean> = _isLogin
@@ -16,20 +19,32 @@ class LogInViewModel @Inject constructor(private val manager: SharedPreferenceMa
     private val _isLoading: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
     val isLoading: LiveData<Boolean> = _isLoading
 
-    fun fetchLoginData() {
-        val token = manager.getString(LOGIN_TOKEN)
-        val name = manager.getString(LOGIN_NAME)
-        val email = manager.getString(LOGIN_EMAIL)
-        val img = manager.getString(LOGIN_IMG)
+    private val _isLogInSuccess: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
+    val isLogInSuccess: LiveData<Boolean> = _isLogInSuccess
 
-        _isLogin.value = token != "" && name != "" && email != "" && img != null
+    fun doLogIn() {
+        val callback = RepoResponseImpl<Unit>()
+        _isLoading.value = true
+
+        callback.addSuccessCallback {
+            _isLogInSuccess.postValue(true)
+            _isLoading.postValue(false)
+        }
+
+        callback.addFailureCallback {
+            _isLogInSuccess.postValue(false)
+            _isLoading.postValue(false)
+        }
+
+        repository.doLogIn(callback)
+    }
+
+    fun fetchLoginData() {
+        _isLogin.value = repository.isAutoLogIn()
     }
 
     fun setLoginData(token: String, name: String, mail: String, img: String) {
-        manager.saveString(LOGIN_NAME, name)
-        manager.saveString(LOGIN_EMAIL, mail)
-        manager.saveString(LOGIN_IMG, img)
-        manager.saveString(LOGIN_TOKEN, token)
+        repository.setLogInData(token, name, mail, img)
     }
 
     fun setLoading(isLoading: Boolean) {
